1b3e1d11266d84d2648b4057b73a521b8bcb710f
Ashish Jha <ashishjha14411@gmail.com>
2022-12-11 22:52:21 +0530

    added the api call with the ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState({})
+  const [text, setText] = useState("")
+  const handleChange = (e) => {
+    let rawText = e.target.value
+    setText(rawText.split(" ").join("+"))
+  }
+  const calldata = async () => {
+    const response = await axios.get(
+      `http://www.omdbapi.com/?t=${text}&apikey=46c1e856`
+    );
+    setData(response.data)
+  };
+
+  return <div className="App">
+    <input 
+    placeholder="Enter the Movie you want to search for"
+    onChange={handleChange}
+    />
+    <button
+    onClick={calldata}
+    >Search</button>
+  </div>;
 }
 
 export default App;
